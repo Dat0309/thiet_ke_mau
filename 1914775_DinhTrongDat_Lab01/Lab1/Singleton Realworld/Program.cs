a64df49024010e7e000f37c00f7defcc4d9e78a2
Dat0309 <anhtheboy011@gmail.com>
2022-03-19 15:52:09 +0700

    update lab01

---
+++ 1914775_DinhTrongDat_Lab01/Lab1/Singleton Realworld/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Realworld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+            // Same instance?
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+            // Load balance 15 server requests
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+    /// <summary>
+    /// The 'Singleton' class
+    /// </summary>
+    public class LoadBalancer
+    {
+        static LoadBalancer instance;
+        List<string> servers = new List<string>();
+        Random random = new Random();
+        private static object locker = new object();
+        protected LoadBalancer()
+        {
+            servers.Add("ServerI");
+            servers.Add("ServerII");
+            servers.Add("ServerIII");
+            servers.Add("ServerIV");
+            servers.Add("ServerV");
+        }
+        public static LoadBalancer GetLoadBalancer()
+        {
+           
+            if (instance == null)
+            {
+                lock (locker)
+                {
+                    if (instance == null)
+                    {
+                        instance = new LoadBalancer();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public string Server
+        {
+            get
+            {
+                int r = random.Next(servers.Count);
+                return servers[r].ToString();
+            }
+        }
+    }
+}
---
+++ 1914775_DinhTrongDat_Lab01/Lab1/Singleton optimized/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Singleton_optimized
+{
+    public class Program
+    {
+        
+        public static void Main()
+        {
+            var b1 = LoadBalancer.GetLoadBalancer();
+            var b2 = LoadBalancer.GetLoadBalancer();
+            var b3 = LoadBalancer.GetLoadBalancer();
+            var b4 = LoadBalancer.GetLoadBalancer();
+            // Confirm these are the same instance
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                WriteLine("Same instance\n");
+            }
+            // Next, load balance 15 requests for a server
+            var balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string serverName = balancer.NextServer.Name;
+                WriteLine("Dispatch request to: " + serverName);
+            }
+            // Wait for user
+            ReadKey();
+        }
+    }
+  
+    public sealed class LoadBalancer
+    {
+        
+        private static readonly LoadBalancer instance = new LoadBalancer();
+        private readonly List<Server> servers;
+        private readonly Random random = new Random();
+        private LoadBalancer()
+        {
+            servers = new List<Server>
+                {
+                  new Server{ Name = "ServerI", IP = "120.14.220.18" },
+                  new Server{ Name = "ServerII", IP = "120.14.220.19" },
+                  new Server{ Name = "ServerIII", IP = "120.14.220.20" },
+                  new Server{ Name = "ServerIV", IP = "120.14.220.21" },
+                  new Server{ Name = "ServerV", IP = "120.14.220.22" },
+                };
+        }
+        public static LoadBalancer GetLoadBalancer()
+        {
+            return instance;
+        }
+        // Simple, but effective load balancer
+        public Server NextServer
+        {
+            get
+            {
+                int r = random.Next(servers.Count);
+                return servers[r];
+            }
+        }
+    }
+   
+    public class Server
+    {
+        public string Name { get; set; }
+        public string IP { get; set; }
+    }
+}
